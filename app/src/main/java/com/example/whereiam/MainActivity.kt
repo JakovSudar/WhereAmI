a11a940de76b9b1b4ae3ef3522721243125d7072
JakovSudar <jakovsudar1@gmail.com>
2020-05-16 15:37:41 +0200

    Refactoring: deleted unused imports

--- app/src/main/java/com/example/whereiam/MainActivity.kt
+++ app/src/main/java/com/example/whereiam/MainActivity.kt
@@ -4,9 +4,6 @@ import android.Manifest
 import android.Manifest.permission.CAMERA
 import android.Manifest.permission.READ_EXTERNAL_STORAGE
 import android.app.Activity
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -16,13 +13,9 @@ import android.os.Build
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
-import android.util.Log
 import android.widget.Toast
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
 import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -48,8 +41,8 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     private var myCity = "noLocation"
 
     //camera
-    val REQUEST_IMAGE_CAPTURE = 1
-    private val PERMISSION_REQUEST_CODE: Int = 101
+    private val REQUEST_IMAGE_CAPTURE = 1
+    private val cameraRequestCode: Int = 101
     private var mCurrentPhotoPath: String? = null
 
 
@@ -90,12 +83,10 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     }
 
     private fun startTrackingLocation() {
-        Log.d("TAG", "Tracking location")
-        val criteria: Criteria = Criteria()
+        val criteria = Criteria()
         criteria.accuracy = Criteria.ACCURACY_FINE
         val provider = locationManager.getBestProvider(criteria, true)
         val minTime = 10L
-
         val minDistance = 10.0F
         try{
             locationManager.requestLocationUpdates(provider, minTime, minDistance, locationListener)
@@ -103,7 +94,6 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
             Toast.makeText(this, "Nemas dozvolu", Toast.LENGTH_SHORT).show()
         }
     }
-
     private val locationListener = object: LocationListener {
         override fun onProviderEnabled(provider: String?) { }
         override fun onProviderDisabled(provider: String?) { }
@@ -117,20 +107,16 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         val lat = location?.latitude ?: 0
         val lon = location?.longitude ?: 0
         val position = LatLng(lat as Double, lon as Double)
-
         myLocation?.remove()
         myLocation = map.addMarker(MarkerOptions().position(position).title("I am here!"))
         map.moveCamera(CameraUpdateFactory.newLatLng(position))
-
         val addresses = geocoder.getFromLocation(lat,lon,1)[0].getAddressLine(0).split(",")
-
         myCity = addresses[2]
         val street = addresses[0]
         val country = addresses[3]
         locationDetails.text =
             "Geogrfska širina: $lat\nGeografska dužina: $lon\nDržava: $country\nGrad: $myCity\nStreet: $street\n"
     }
-
     override fun onMapReady(googleMap: GoogleMap) {
         trackLocation()
         map = googleMap
@@ -143,7 +129,6 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
                 .icon(BitmapDescriptorFactory.fromResource(R.drawable.marker)))
         }
     }
-
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
@@ -155,7 +140,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
                 else
                     Toast.makeText(this,"Permission Denied", Toast.LENGTH_SHORT).show()
             }
-            PERMISSION_REQUEST_CODE -> {
+            cameraRequestCode -> {
                 if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)
                     &&grantResults[1] == PackageManager.PERMISSION_GRANTED) {
                     takePicture()
@@ -167,7 +152,6 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
             else -> super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         }
     }
-
 
     override fun onDestroy() {
         super.onDestroy()
@@ -187,6 +171,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
             mCurrentPhotoPath = absolutePath
         }
     }
+
     private fun takePicture() {
         val intent: Intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
         val file: File = createFile()
@@ -199,6 +184,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         intent.putExtra(MediaStore.EXTRA_OUTPUT,uri)
         startActivityForResult(intent, REQUEST_IMAGE_CAPTURE)
     }
+
     private fun checkPersmission(): Boolean {
         return (ContextCompat.checkSelfPermission(this, CAMERA) ==
                 PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this,
@@ -208,14 +194,13 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private fun requestPermission() {
         ActivityCompat.requestPermissions(this, arrayOf(READ_EXTERNAL_STORAGE, CAMERA),
-            PERMISSION_REQUEST_CODE)
+            cameraRequestCode)
     }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
            notification.displayImageNotification()
         }
     }
-    //notifikacija
-
 }
